28f05ba77788e8646d99287fe95a58a902a96d5f
Guru Raghupathy <guru@doit-intl.com>
2024-09-18 12:08:28 +0100

    Initial Checkin

---
+++ src/main/java/com/csdm/poc/gradlegcpgardemo/GradlegcpgardemoApplication.java
@@ -0,0 +1,13 @@
+package com.csdm.poc.gradlegcpgardemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradlegcpgardemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradlegcpgardemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csdm/poc/gradlegcpgardemo/GradlegcpgardemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csdm.poc.gradlegcpgardemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradlegcpgardemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
